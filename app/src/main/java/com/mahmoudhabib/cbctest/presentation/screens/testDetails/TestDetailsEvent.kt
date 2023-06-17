089c94222c3221c7e2793262f8dede9f735a5b70
mhmoudhabib <mhmoudrhabib@gmail.com>
2023-06-18 02:13:20 +0300

    report generation and share

--- app/src/main/java/com/mahmoudhabib/cbctest/presentation/screens/testDetails/TestDetailsEvent.kt
+++ app/src/main/java/com/mahmoudhabib/cbctest/presentation/screens/testDetails/TestDetailsEvent.kt
@@ -5,4 +5,6 @@ sealed class TestDetailsEvent {
     object ShowDeleteConfirmationDialog : TestDetailsEvent()
     object DeleteTestDetails : TestDetailsEvent()
     object DismissDeletion : TestDetailsEvent()
+    object ShareTestDetails : TestDetailsEvent()
+    object ClearPdf : TestDetailsEvent()
 }
--- app/src/main/java/com/mahmoudhabib/cbctest/presentation/screens/testDetails/TestDetailsViewModel.kt
+++ app/src/main/java/com/mahmoudhabib/cbctest/presentation/screens/testDetails/TestDetailsViewModel.kt
@@ -1,5 +1,7 @@
 package com.mahmoudhabib.cbctest.presentation.screens.testDetails
 
+import android.app.Application
+import android.net.Uri
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
@@ -7,20 +9,29 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mahmoudhabib.cbctest.domain.model.TestResult
 import com.mahmoudhabib.cbctest.domain.usecases.TestResultsUseCases
+import com.mahmoudhabib.cbctest.presentation.PdfUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
 
 @HiltViewModel
 class TestDetailsViewModel @Inject constructor(
     private val useCases: TestResultsUseCases,
     private val savedStateHandle: SavedStateHandle,
+    private val application: Application,
 ) :
     ViewModel() {
     val testDetails = mutableStateOf<TestResult?>(null)
 
-    private val _isLoading = mutableStateOf<Boolean>(true)
-    val isLoading: State<Boolean> = _isLoading
+    private val _isCreatingPdfFile = mutableStateOf<Boolean>(false)
+    val isCreatingPdfFile: State<Boolean> = _isCreatingPdfFile
+
+    private val _isPdfFileCreated = mutableStateOf<Boolean>(false)
+    val isPdfFileCreated: State<Boolean> = _isPdfFileCreated
+
+    private val _pdfFileUri = mutableStateOf<Uri?>(null)
+    val pdfFileUri: State<Uri?> = _pdfFileUri
 
     private val _alertDialogState = mutableStateOf<Boolean>(false)
     val alertDialogState: State<Boolean> = _alertDialogState
@@ -28,16 +39,15 @@ class TestDetailsViewModel @Inject constructor(
     private val _isDeleteSuccess = mutableStateOf<Boolean>(false)
     val isDeleteSuccess: State<Boolean> = _isDeleteSuccess
 
+
     fun onEvent(event: TestDetailsEvent) {
         when (event) {
             is TestDetailsEvent.LoadTestDetails -> {
                 savedStateHandle.get<Int>("testID")?.let { testID ->
                     viewModelScope.launch {
-                        _isLoading.value = true
                         useCases.getTestDetails(testID)?.let {
                             testDetails.value = it
                         }
-                        _isLoading.value = false
                     }
                 }
             }
@@ -54,6 +64,23 @@ class TestDetailsViewModel @Inject constructor(
                 deleteTestResult()
                 _alertDialogState.value = false
                 _isDeleteSuccess.value = true
+            }
+
+            TestDetailsEvent.ShareTestDetails -> {
+                testDetails.value?.let { test ->
+                    _isCreatingPdfFile.value = true
+                    _isPdfFileCreated.value = false
+                    val pdfUri = PdfUtils.createReport(application.applicationContext, test)
+                    _pdfFileUri.value = pdfUri
+                    _isCreatingPdfFile.value = false
+                    _isPdfFileCreated.value = true
+                }
+            }
+
+            TestDetailsEvent.ClearPdf -> {
+                _pdfFileUri.value = null
+                _isPdfFileCreated.value = false
+                _isCreatingPdfFile.value = false
             }
         }
 
